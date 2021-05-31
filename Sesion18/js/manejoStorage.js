a814bde942487f9a4b9c776348f53543fe71c262
mage <marianavarromonjeeugenia@gmail.com>
2021-05-31 12:34:40 -0500

    CambiosSesion22

---
+++ Sesion18/js/manejoStorage.js
@@ -0,0 +1,13 @@
+function guardardatos(){
+    localStorage.nombre=document.getElementById("nombre").value;
+    localStorage.movil=document.getElementById("movil").value;
+}
+
+function recuperardatos(){
+    if((localStorage.nombre!=undefined) && (localStorage.movil!=undefined)){
+        document.getElementById("datos").innerHTML="Nombre: "+ localStorage.nombre + " <br> Num celular:" + localStorage.movil;
+    } else {
+        document.getElementById("datos").innerHTML="No has ingresado datos"
+    }
+}
+
